dd8d19042f704421bb56f5c6c594d5a0c92b8194
Shourya Goyal <90088878+shouryagoyal123456@users.noreply.github.com>
2023-05-09 14:30:38 +0530

    Add files via upload

---
+++ HotelManagemetnSystem/src/main/java/com/example/HotelManagemetnSystem/HotelManagemetnSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.HotelManagemetnSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagemetnSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagemetnSystemApplication.class, args);
+	}
+
+}
---
+++ HotelManagemetnSystem/src/test/java/com/example/HotelManagemetnSystem/HotelManagemetnSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HotelManagemetnSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagemetnSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
